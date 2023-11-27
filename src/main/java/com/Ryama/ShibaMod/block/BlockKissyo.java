fb068279618fa0479018e7ed9408946e584f83dd
RyumaRyama <ryumaryama@gmail.com>
2023-11-28 02:04:14 +0900

    板状の当たり判定を追加

--- src/main/java/com/Ryama/ShibaMod/block/BlockKissyo.java
+++ src/main/java/com/Ryama/ShibaMod/block/BlockKissyo.java
@@ -1,11 +1,18 @@
 package com.Ryama.ShibaMod.block;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class BlockKissyo extends Block {
     public BlockKissyo() {
@@ -25,5 +32,23 @@ public class BlockKissyo extends Block {
     public BlockState getStateForPlacement(BlockPlaceContext context) {
         return this.defaultBlockState().setValue(FACING,
                 context.getHorizontalDirection().getOpposite());
+    }
+
+    // ブロックの当たり判定を設定
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+        Direction direction = state.getValue(FACING);
+        switch (direction) {
+            case NORTH:
+                return Shapes.create(new AABB(0.0, 0.0, 0.9, 1.0, 1.0, 1.0));
+            case SOUTH:
+                return Shapes.create(new AABB(0.0, 0.0, 0.0, 1.0, 1.0, 0.1));
+            case WEST:
+                return Shapes.create(new AABB(0.9, 0.0, 0.0, 1.0, 1.0, 1.0));
+            case EAST:
+                return Shapes.create(new AABB(0.0, 0.0, 0.0, 0.1, 1.0, 1.0));
+            default:
+                return Shapes.block();
+        }
     }
 }
